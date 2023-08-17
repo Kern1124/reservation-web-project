74115060e132c8eb3666867159d8cd96b599a557
xjanos <xjanos@fi.muni.cz>
2023-08-17 19:42:22 +0200

    feat(sidebar): added the option to retract the sidebar

--- frontend/src/app/sidebar/sidebar.component.ts
+++ frontend/src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SidebarService } from './sidebar.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class SidebarComponent {
 
+  retracted: boolean = false;
+
+  constructor(private sidebarService: SidebarService){}
+
+  retractSidebar(){
+    this.retracted = !this.retracted;
+  }
+  
 }
